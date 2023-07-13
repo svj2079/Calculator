f4af0ca800d2ff41f4844425bb0a6ad5e5467625
sahar <sahar.vahedi99@gmail.com>
2023-07-13 11:51:16 +0330

    first commit

---
+++ mymodule.py
@@ -0,0 +1,9 @@
+def greeting(name):
+    print("hello, " + name)
+
+person1 = {
+    "fname": "sahar",
+    "lname": "vahedi",
+    "age": 23,
+    "country": "iran"
+}
---
+++ t1.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "sahar",
+    password = "sahar"
+)
+
+print(mydb)
+
+
+
+
+
+
+
+
+
+
+
+
+#regex python
+#function finall
