03d5d17c8d9eae661a0ca81462887313490210e5
sahar <sahar.vahedi99@gmail.com>
2023-07-13 12:51:35 +0330

    create buttons

--- calculator.py
+++ calculator.py
@@ -9,7 +9,40 @@ class Calculator:
 
         self.equation = Entry(master, width=36, borderwidth=5)
         self.equation.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
-         
+        
+
+    def createButton(self):
+
+        b0 = self.addButton(0)
+        b1 = self.addButton(1)
+        b2 = self.addButton(2)
+        b3 = self.addButton(3)
+        b4 = self.addButton(4)
+        b5 = self.addButton(5)
+        b6 = self.addButton(6)
+        b7 = self.addButton(7)
+        b8 = self.addButton(8)
+        b9 = self.addButton(9)
+        b_add = self.addButton('+')
+        b_sub = self.addButton('-')
+        b_mult = self.addButton('*')
+        b_div = self.addButton('/')
+        b_clear = self.addButton('c')
+        b_equal = self.addButton('=')
+
+        row1 = [b7, b8, b9, b_add]
+        row2 = [b4, b5, b6, b_sub]
+        row3 = [b1, b2, b3, b_mult]
+        row4 = [b_clear, b0, b_equal, b_div]
+
+        r = 1
+        for row in [row1, row2, row3, row4]:
+            c = 0
+            for buttn in row:
+                buttn.grid(row=r, column=c, columnspan=1)
+                c+=1
+            r+=1
+            
 
 if __name__=='__main__':
 
